137b3afa8aabaf6d001998db6d8a0813ffa72f32
17082 <17082@10.100.210.109>
2022-11-16 11:02:29 -0600

    PE6.4v2

--- VicentePina_2200_PE6.4/src/Exercise06_04.java
+++ VicentePina_2200_PE6.4/src/Exercise06_04.java
@@ -1,26 +1,32 @@
 /**
- * 
+ *
  * @author Vicente Pina
- *Date:11/14/2022 [M]
+ *Date:11/16/2022 [M]
  *
  */
 import java.util.Scanner;
 public class Exercise06_04 {
-	public static void main(String[] args) {
-	System.out.print("Enter an integer: ");
-	Scanner input = new Scanner(System.in);
-	int number = input.nextInt();
-	reverse(number);	
-	input.close();
-	}
-	
-	public static void reverse(int n) {
-		if (n==0) {
-			System.out.print(n);
-		}
-		while(n != 0) {
-			System.out.print(n % 10);
-			n= n / 10;
-		}
-	}
+public static void main(String[] args) {
+System.out.print("Enter an integer: ");
+Scanner input = new Scanner(System.in);
+int number = input.nextInt();
+reverse(number);
+input.close();
+}
+
+public static void reverse(int n) {
+if (n==0) {
+System.out.print(n);
+}
+while (n<0) {
+System.out.print("-");
+System.out.print((n = n*-1) % 10);
+n= n / 10;
+}
+
+while(n > 0) {
+System.out.print(n % 10);
+n= n / 10;
+}
+}
 }
